39d8e6614a331b67e7057b279b9e3034ba487c34
Halyna <galina14b@gmail.com>
2023-09-05 11:32:29 +0300

    final

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -87,7 +87,7 @@ const Modal = ({ onClick }) => {
       <div className={css.modal__wrapper}>
         <button className={css.closeBtn} onClick={onClick}>
           <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" clipRule="evenodd" d="M8.05716 8.99997L0.528564 1.47137L1.47137 0.528564L8.99997 8.05716L16.5286 0.528564L17.4714 1.47137L9.94278 8.99997L17.4714 16.5286L16.5286 17.4714L8.99997 9.94278L1.47137 17.4714L0.528564 16.5286L8.05716 8.99997Z" fill="#FF868E"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M8.05716 8.99997L0.528564 1.47137L1.47137 0.528564L8.99997 8.05716L16.5286 0.528564L17.4714 1.47137L9.94278 8.99997L17.4714 16.5286L16.5286 17.4714L8.99997 9.94278L1.47137 17.4714L0.528564 16.5286L8.05716 8.99997Z" fill=""/>
           </svg>
         </button>
         <h1 className={css.modal__title}>Upload a .jpg or .png Cat Image</h1>
