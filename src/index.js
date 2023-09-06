a719e994f760e68f63bcc47ecbd689dc8a680ca7
Halyna <galina14b@gmail.com>
2023-09-06 07:19:19 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/TestMacPaw">
+    <BrowserRouter basename="/PetsPawApp">
       <App />
     </BrowserRouter>
   </React.StrictMode>
